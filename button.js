bcaf36b4d936fefbbb5d670bd971f64d2ce1d548
Cayman Gill <cayman.g@hotmail.com>
2021-11-28 12:27:06 -0500

    starting refractor for live site

---
+++ button.js
@@ -0,0 +1,16 @@
+var stat;
+function buttonPush()
+{
+    document.getElementById('button').setAttribute('src','assets/button-push.png')
+}
+
+function buttonUnpush()
+{
+    document.getElementById('button').setAttribute('src','assets/button.png')
+}
+
+/*function butttonStats()
+{
+    stat = 1++;
+    document.getElementById('stat').textContent(stat.toString)
+}*/
